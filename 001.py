ce2263ae3b9dda32271d33fb20b591509e5835d8
WDdeBWT <bwt1997@126.com>
2017-09-25 22:59:14 +0800

    0925-结巴分词上手

---
+++ 001.py
@@ -0,0 +1,12 @@
+﻿# encoding=utf-8
+import jieba
+
+ReadPath = "D:\\2016ab.txt"
+wordList = []
+with open(ReadPath, 'r') as r:
+	for line in r.readlines():
+		words = jieba.cut(line)
+		for word in words:
+			wordList.append(word)
+		#print("Default Mode: " + "/ ".join(words))  # 精确模式
+	print("Default Mode: " + "/ ".join(wordList))
